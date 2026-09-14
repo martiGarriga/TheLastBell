4b2c69d465dab8fe25e05b1e375d68368f2dac71
martiGarriga <mgarrigal@edu.tecnocampus.cat>
2026-09-14 22:41:45 +0200

    NPC implementation

---
+++ TheLastBell/TheLastBell/NPC.cpp
@@ -0,0 +1,17 @@
+#include "NPC.h"
+
+NPC::NPC(const std::string& name, const std::string& description, Room* location) : Creature(EntityType::NPC, name, description, location), nextLine(0) {}
+
+void NPC::AddDialogueLine(const std::string& line) {
+	dialogueScript.push_back(line);
+}
+
+std::string NPC::Talk() {
+	if (dialogueScript.empty()) {
+		return GetName() + " He/She has nothing to say."; //Implementar genero
+	}
+
+	const std::string& line = dialogueScript[nextLine];
+	nextLine = (nextLine + 1) % dialogueScript.size();
+	return line;
+}
---
+++ TheLastBell/TheLastBell/NPC.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "Creature.h"
+#include <vector>
+#include <string>
+
+class NPC : public Creature {
+private:
+	std::vector<std::string> dialogueScript;
+	size_t nextLine;
+
+public:
+	NPC(const std::string& name, const std::string& description, Room* location);
+
+	void AddDialogueLine(const std::string& line);
+
+	std::string Talk();
+
+	void Update() override;
+};
